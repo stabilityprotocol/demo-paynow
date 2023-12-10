9a5929fe37efcd8c8700ce1ad1e7aa0f45185cdc
Daniel Blanco Parla <dblancoparla@gmail.com>
2023-12-10 20:11:46 +0100

    feat: update contract source code

--- contracts/contracts/Faucet.sol
+++ contracts/contracts/Faucet.sol
@@ -1,7 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Faucet {
     IERC20 public token;
+
+    event TokenDistribution(address target, uint256 amount);
 
     constructor(address _token) {
         token = IERC20(_token);
@@ -9,7 +14,7 @@ contract Faucet {
 
     function getTokens() public {
         uint256 amount = (block.number % 100) * 100 ether;
-
         token.transfer(msg.sender, amount);
+        emit TokenDistribution(msg.sender, amount);
     }
 }
--- contracts/contracts/PayUSD.sol
+++ contracts/contracts/PayUSD.sol
@@ -1,7 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract PayUSD is ERC20 {
-    constructor() ERC20("PayUSD", "PUSD") {
+contract PyUSD is ERC20 {
+    constructor() ERC20("PayPal USD", "PYUSD") {
         _mint(msg.sender, 10000000 ether);
     }
 }
