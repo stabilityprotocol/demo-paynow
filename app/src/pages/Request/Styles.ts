4b0e12df59fe56343a701992404ac34a44adfc2d
Carlos Lostao <hastro1990@gmail.com>
2023-12-11 17:48:01 +0100

    update: margin in send view

--- app/src/pages/Request/Styles.ts
+++ app/src/pages/Request/Styles.ts
@@ -1,33 +1,33 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ButtonWrapper = styled.div`
-    width: 80%;
-    gap: 10px;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
+  width: 80%;
+  gap: 10px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 `;
 
 export const PageTitle = styled.h1`
-    font-family: ${(props) => props.theme.font.primary};
-    font-weight: 500;
-    font-size: ${(props) => props.theme.fontSizes.xlarge};
-    margin-bottom: ${(props) => props.theme.spacing.large};
+  font-family: ${(props) => props.theme.font.primary};
+  font-weight: 500;
+  font-size: ${(props) => props.theme.fontSizes.xlarge};
+  margin-bottom: ${(props) => props.theme.spacing.large};
 `;
 
 export const AttributeWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: ${(props) => props.theme.spacing.medium};
-    margin-top: ${(props) => props.theme.spacing.large};
-    margin-bottom: ${(props) => props.theme.spacing.xlarge};
-    width: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.theme.spacing.medium};
+  margin-top: ${(props) => props.theme.spacing.medium};
+  margin-bottom: ${(props) => props.theme.spacing.large};
+  width: 80%;
 `;
 
 export const AddMemoWrapper = styled.div`
-    height: 6.375rem;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  height: 6.375rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
