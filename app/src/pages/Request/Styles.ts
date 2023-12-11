1fe96af327706b56d41769a1c17e404cedfdc283
GabrielMartinezRodriguez <gabrielmartinezrdrgz@gmail.com>
2023-12-11 12:59:02 +0100

    chore: requesting improvements

--- app/src/pages/Request/Styles.ts
+++ app/src/pages/Request/Styles.ts
@@ -7,3 +7,27 @@ export const ButtonWrapper = styled.div`
     justify-content: center;
     flex-direction: column;
 `;
+
+export const PageTitle = styled.h1`
+    font-family: ${(props) => props.theme.font.primary};
+    font-weight: 500;
+    font-size: ${(props) => props.theme.fontSizes.xlarge};
+    margin-bottom: ${(props) => props.theme.spacing.large};
+`;
+
+export const AttributeWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: ${(props) => props.theme.spacing.medium};
+    margin-top: ${(props) => props.theme.spacing.large};
+    margin-bottom: ${(props) => props.theme.spacing.xlarge};
+    width: 80%;
+`;
+
+export const AddMemoWrapper = styled.div`
+    height: 6.375rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
--- app/src/pages/Request/index.tsx
+++ app/src/pages/Request/index.tsx
@@ -1,19 +1,26 @@
 import { Button, ButtonNoFilled } from "../../components/Button";
 import { UserIcon } from "../../components/UserIcon";
-import { ButtonWrapper } from "./Styles";
+import { AttributeWrapper, ButtonWrapper, PageTitle, AddMemoWrapper } from "./Styles";
 import { AddMemo } from "./components/AddMemo";
 import { Quantity } from "./components/Quantity";
 import { RequestAttribute } from "./components/RequestAttribute";
 
 export const Request = () => {
   return <>
-          <h1>REQUESTING</h1>
+          <PageTitle>REQUESTING</PageTitle>
            <UserIcon name="gabriel.stability" letters="GS"/>
            <Quantity quantity={100}/>
-           <AddMemo/>
-           <RequestAttribute name="Stability Fee" value="0"/>
-           <RequestAttribute name="Your total" value="15"/>
-           <RequestAttribute name="Payment recieved" value="In Second"/>
+           <AddMemoWrapper>
+            <AddMemo/>
+           </AddMemoWrapper>
+
+
+           <AttributeWrapper>
+            <RequestAttribute name="Fee" value="0"/>
+            <RequestAttribute name="Total" value="15"/>
+            <RequestAttribute name="Txn Completed" value="In Second"/>
+           </AttributeWrapper>
+
 
 
             <ButtonWrapper>
