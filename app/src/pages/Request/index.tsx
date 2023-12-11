abb056c98923dde15d280f8d5ed9a9b7158b4142
GabrielMartinezRodriguez <gabrielmartinezrdrgz@gmail.com>
2023-12-11 15:18:12 +0100

    chore: handle confirm button

--- app/src/pages/Request/index.tsx
+++ app/src/pages/Request/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { usePaymentRequest } from "../../common/hooks/usePaymentRequest";
 import { Button, ButtonNoFilled } from "../../components/Button";
 import { UserIcon } from "../../components/UserIcon";
 import { AttributeWrapper, ButtonWrapper, PageTitle, AddMemoWrapper } from "./Styles";
@@ -6,12 +8,24 @@ import { Quantity } from "./components/Quantity";
 import { RequestAttribute } from "./components/RequestAttribute";
 
 export const Request = () => {
+  const [memo, setMemo] = useState('');
+
+  const { createRequest } = usePaymentRequest();
+
+  const handleCreateRequest = () => {
+    createRequest(
+      "0x8eB92711aa4Fa5D54fD41dB4A47C012bD66783cF",
+      "100",
+      memo,
+    );
+  };
+
   return <>
           <PageTitle>REQUESTING</PageTitle>
            <UserIcon name="gabriel.stability" letters="GS"/>
            <Quantity quantity={100}/>
            <AddMemoWrapper>
-            <AddMemo/>
+            <AddMemo value={memo} onChange={(e) => setMemo(e.target.value)}/>
            </AddMemoWrapper>
 
 
@@ -24,7 +38,7 @@ export const Request = () => {
 
 
             <ButtonWrapper>
-              <Button>CONFIRM</Button>
+              <Button onClick={handleCreateRequest}>CONFIRM</Button>
               <ButtonNoFilled>CANCEL</ButtonNoFilled>
             </ButtonWrapper>
 
