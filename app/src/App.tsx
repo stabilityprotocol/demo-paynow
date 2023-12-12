5539d271fa073a09ee661121c8672c1b4dfd9472
Carlos Lostao <hastro1990@gmail.com>
2023-12-12 15:50:45 +0100

    update: title section

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -21,7 +21,7 @@ import { AddMoney } from "./pages/AddMoney";
 import { ComeLater } from "./pages/AddMoney/ComeLater";
 import { SetUsername } from "./pages/SetUsername";
 import { Updater } from "./components/Updater";
-import { Search } from "./pages/Search";
+import { Transfer } from "./pages/Search";
 import { TransferAddress } from "./pages/TransferAddress";
 import { PayRequest } from "./pages/PayRequest";
 
@@ -64,7 +64,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/transfer",
-        element: <Search />,
+        element: <Transfer />,
       },
       {
         path: "/transfer/address",
