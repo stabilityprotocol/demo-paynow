7454cf16fd42a05e9d116285c789b3700f66b26a
Daniel Blanco Parla <dblancoparla@gmail.com>
2023-12-13 19:59:15 +0100

    chore(HeaderMobile): don't show notification when no user

--- app/src/components/HeaderMobile/index.tsx
+++ app/src/components/HeaderMobile/index.tsx
@@ -5,17 +5,19 @@ import { FaUserCircle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { UserState } from "../../common/State/User";
+import { useAccount } from "wagmi";
 
 export const HeaderMobile: React.FC = () => {
   const navigate = useNavigate();
   const userState = useRecoilValue(UserState);
+  const { address } = useAccount();
 
   return (
     <HeaderMobileWrapper>
       <img src={Logo} alt="PayNow" onClick={() => navigate("/balance")} />
       <HeaderMobileProfileWrapper>
         <FaUserCircle onClick={() => navigate("/profile")} />
-        {!userState.ens && <span className="notification"></span>}
+        {!userState.ens && address && <span className="notification"></span>}
       </HeaderMobileProfileWrapper>
     </HeaderMobileWrapper>
   );
