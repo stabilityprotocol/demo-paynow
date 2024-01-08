bccca948c167f69d2895a1ad2562bb02841da50b
Daniel Blanco Parla <dblancoparla@gmail.com>
2024-01-08 16:14:18 +0100

    feat(Token): Bolt token

--- app/src/common/Blockchain/Contracts.ts
+++ app/src/common/Blockchain/Contracts.ts
@@ -8,6 +8,10 @@ export const Contracts: Record<
     token: "0xb253fAA010eC8DfC976232BF20c4fd340740A3Bf",
     paymentRequests: "0xC5C81a0879d5453F5b3c783f9C77FFAdF2F25420",
   },
+  bolt: {
+    token: "0x98A4cf3e95F80e38922A6D702Ce9Cb8C09fE6439",
+    paymentRequests: "0x120D1c2d71DEcf1B1ACd49B8a855e9bb7ff24589",
+  },
 };
 
 export const EnsContract: Address =
